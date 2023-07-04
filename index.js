fddb207d6370b1cd60ee3fc3ea60e0a12e501b98
Fruit Juice <c.erinmcgrane1@gmail.com>
2023-07-04 19:08:36 -0400

    deleted examples folder

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function getInput() {
     }]).then((answer) => {
         let shape;
 
-        // write an if statement for passing the input to the correct shape constructor
+        // write an if statement for passing the input to the correct shape constructor and create that shape
         if (answer.shape === 'Circle') {
             shape = new Circle(answer.text, answer.textColor, answer.shapeColor)
         }
@@ -35,8 +35,7 @@ function getInput() {
         if (answer.shape === 'Rectangle') {
             shape = new Rectangle(answer.text, answer.textColor, answer.shapeColor)
         }
-        // pass answers to the constructor method
-
+        // create a new logo.svg file with the return from render
         fs.writeFile('./dist/logo.svg', shape.render(), (err, result) => {
             if (err) throw err;
             console.log('svg created!');
